ccb62891d7ef7ceadbb2a8644b53d7f73810de8d
Suleyman Kanal <kanalSuleyman@outlook.com>
2025-02-27 01:01:23 +0300

    Add health check for Catalog microservice with PostgresDb

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,38 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+    config.RegisterServicesFromAssembly(assembly)
+        .AddOpenBehavior(typeof(ValidationBehavior<,>))
+        .AddOpenBehavior(typeof(LoggingBehavior<,>))
+);
+
 builder.Services.AddCarter();
-builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 builder.Services.AddMarten(builder.Configuration.GetConnectionString("Database")!).UseLightweightSessions();
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+}
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
+
+app.UseHealthChecks("/health", 
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.MapCarter();
 
