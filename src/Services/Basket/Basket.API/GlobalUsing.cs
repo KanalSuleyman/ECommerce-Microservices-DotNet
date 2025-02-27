7b6fb91ee4c6cfaef4324e89ce45021db5b43e1d
Suleyman Kanal <kanalSuleyman@outlook.com>
2025-02-27 23:24:41 +0300

    Create GlobalUsing file

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,13 @@
+﻿global using Mapster;
+global using MediatR;
+global using Carter;
+global using Marten;
+global using FluentValidation;
+
+global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Exceptions;
+global using BuildingBlocks.Behaviors;
+
+global using Basket.API.Exceptions;
+global using Basket.API.Models;
+global using Basket.API.Data;
