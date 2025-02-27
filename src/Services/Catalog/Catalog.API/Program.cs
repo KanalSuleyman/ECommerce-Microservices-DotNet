85103c3afd0ad90020af2b5228ea4be299873e6b
Suleyman Kanal <kanalSuleyman@outlook.com>
2025-02-27 23:37:06 +0300

    Perform code refactoring

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -6,10 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
 
 builder.Services.AddMediatR(config =>
-    config.RegisterServicesFromAssembly(assembly)
-        .AddOpenBehavior(typeof(ValidationBehavior<,>))
-        .AddOpenBehavior(typeof(LoggingBehavior<,>))
-);
+    {
+        config.RegisterServicesFromAssembly(assembly);
+        config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+    });
 
 builder.Services.AddCarter();
 
