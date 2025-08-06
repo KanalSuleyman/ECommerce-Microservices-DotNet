77eab9577bb7212edbc0447ddfbd145012e66973
KanalSuleyman <kanalSuleyman@outlook.com>
2025-08-06 06:30:54 +0300

    Add strongly typed IDs for Order entities and add rich domain model methods. Create OrderCreatedEvent and OrderUpdatedEvent records.

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -1,17 +1,18 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class OrderItem : Entity<Guid>
+public class OrderItem : Entity<OrderItemId>
 {
-    internal OrderItem(Guid orderId, Guid orderItemId, int quantity, decimal price)
+    internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
     {
+        Id = OrderItemId.Of(Guid.NewGuid());
         OrderId = orderId;
-        OrderItemId = orderItemId;
+        ProductId = productId;
         Quantity = quantity;
         Price = price;
     }
 
-    public Guid OrderId { get; private set; }
-    public Guid OrderItemId { get; private set; }
+    public OrderId OrderId { get; private set; }
+    public ProductId ProductId { get; private set; }
     public int Quantity { get; private set; }
     public decimal Price { get; private set; }
 }
