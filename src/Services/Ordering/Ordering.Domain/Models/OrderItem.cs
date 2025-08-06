4c16a05db2bc580a31542ff74adb089758e1bfa6
KanalSuleyman <kanalSuleyman@outlook.com>
2025-08-06 04:38:55 +0300

    Develop domain modeling with models, value objects and enums

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Domain.Models;
+
+public class OrderItem : Entity<Guid>
+{
+    internal OrderItem(Guid orderId, Guid orderItemId, int quantity, decimal price)
+    {
+        OrderId = orderId;
+        OrderItemId = orderItemId;
+        Quantity = quantity;
+        Price = price;
+    }
+
+    public Guid OrderId { get; private set; }
+    public Guid OrderItemId { get; private set; }
+    public int Quantity { get; private set; }
+    public decimal Price { get; private set; }
+}
